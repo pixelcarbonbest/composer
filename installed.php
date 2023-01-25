339aad762c34fbc658d7ec5dd09b28bc7662b27d
pixelcarbonbest <116330515+pixelcarbonbest@users.noreply.github.com>
2023-01-25 04:51:16 +0100

    php json

---
+++ installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f13629cf4a091f0d7307e4daed15eae31acf1134',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f13629cf4a091f0d7307e4daed15eae31acf1134',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
